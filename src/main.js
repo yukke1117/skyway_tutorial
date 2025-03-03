f105344e9f6e02f95ddc320e753f947ad90dff19
yukke04500 <akagiyusuke@icloud.com>
2025-03-03 21:22:02 +0900

    tutorial終わった

---
+++ src/main.js
@@ -0,0 +1,103 @@
+import { nowInSec, SkyWayAuthToken, SkyWayContext, SkyWayRoom, SkyWayStreamFactory, uuidV4 } from "@skyway-sdk/room";
+
+
+const token = new SkyWayAuthToken({
+    jti: uuidV4(),
+    iat: nowInSec(),
+    exp: nowInSec() + 60 * 60 * 24,
+    version: 3,
+    scope: {
+      appId: "x6PjpSFPAiJZhsgIzrpzULlA6613MgwmyORxDduLgw4=",
+      rooms: [
+        {
+          name: "*",
+          methods: ["create", "close", "updateMetadata"],
+          member: {
+            name: "*",
+            methods: ["publish", "subscribe", "updateMetadata"],
+          },
+        },
+      ],
+    },
+}).encode("x6PjpSFPAiJZhsgIzrpzULlA6613MgwmyORxDduLgw4=");
+  
+(async () => {
+    const localVideo = document.getElementById("local-video");
+    const buttonArea = document.getElementById("button-area");
+    const remoteMediaArea = document.getElementById("remote-media-area");
+    const roomNameInput = document.getElementById("room-name");
+  
+    const myId = document.getElementById("my-id");
+    const joinButton = document.getElementById("join");
+    const leaveButton = document.getElementById("leave");
+  
+    const { audio, video } =
+      await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
+    video.attach(localVideo);
+    await localVideo.play();
+  
+    joinButton.onclick = async () => {
+      if (roomNameInput.value === "") return;
+  
+      const context = await SkyWayContext.Create(token);
+      const room = await SkyWayRoom.FindOrCreate(context, {
+        type: "p2p",
+        name: roomNameInput.value,
+      });
+      const me = await room.join();
+  
+      myId.textContent = me.id;
+  
+      await me.publish(audio);
+      await me.publish(video);
+  
+      const subscribeAndAttach = (publication) => {
+        if (publication.publisher.id === me.id) return;
+  
+        const subscribeButton = document.createElement("button");
+        subscribeButton.id = `subscribe-button-${publication.id}`;
+        subscribeButton.textContent = `${publication.publisher.id}: ${publication.contentType}`;
+        buttonArea.appendChild(subscribeButton);
+  
+        subscribeButton.onclick = async () => {
+          const { stream } = await me.subscribe(publication.id);
+  
+          let newMedia;
+          switch (stream.track.kind) {
+            case "video":
+              newMedia = document.createElement("video");
+              newMedia.playsInline = true;
+              newMedia.autoplay = true;
+              break;
+            case "audio":
+              newMedia = document.createElement("audio");
+              newMedia.controls = true;
+              newMedia.autoplay = true;
+              break;
+            default:
+              return;
+          }
+          newMedia.id = `media-${publication.id}`;
+          stream.attach(newMedia);
+          remoteMediaArea.appendChild(newMedia);
+        };
+      };
+  
+      room.publications.forEach(subscribeAndAttach);
+      room.onStreamPublished.add((e) => subscribeAndAttach(e.publication));
+  
+      leaveButton.onclick = async () => {
+        await me.leave();
+        await room.dispose();
+  
+        myId.textContent = "";
+        buttonArea.replaceChildren();
+        remoteMediaArea.replaceChildren();
+      };
+  
+      room.onStreamUnpublished.add((e) => {
+        document.getElementById(`subscribe-button-${e.publication.id}`)?.remove();
+        document.getElementById(`media-${e.publication.id}`)?.remove();
+      });
+    };
+})();
